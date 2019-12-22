7acd13ffebc9f1021f6b92aed5c9d27f912e2eee
Alex Merced - AlexMercedCoder.com <alexmerced@alexmerced.com>
2019-12-22 09:57:09 -0500

    first commit pre heroku deployment

---
+++ layouts/default.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <a href="https://www.devnursery.com"><h1 class="logo"><span>dev</span>Nursery</h1></a>
+    <nuxt />
+    <h4>This application is made by Alex Merced of AlexMerced.com using Vue/Nuxt</h4>
+  </div>
+
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Major+Mono+Display&display=swap');
+
+body {
+    text-align: center;
+    padding: 20px;
+    }
+
+.logo {
+        text-align: center;
+        font-size: 300%;
+        width: 100%;
+        margin: 10px auto;
+        font-family: 'Major Mono Display', monospace;
+    }
+
+.logo span {
+        color: #A5243D;
+    }
+
+a {text-decoration: none; color: black;}
+
+@media only screen and (max-width: 500px) {
+  .logo {font-size: 175%;}
+}
+
+</style>
---
+++ pages/_user.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="container">
+    <h1>{{user}} Blog Posts</h1>
+    <div class='posts'>
+        <div v-for="post in posts" class="post">
+            <h1>{{post.title}}</h1>
+            <h3>Featured Tech: {{post.tech}}</h3>
+            <button v-on:click="switchShow(post._id)">Show</button>
+            <p v-if="switches[post._id]">{{post.entry}}</p>
+
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+// const getPosts = async () => {
+//     const url = 'https://wrfjnsifxl.execute-api.us-east-1.amazonaws.com/production/api/blogs/'
+//     const response = await fetch(url)
+//     const json = await response.json()
+//     console.log(json)
+//     return await json
+//   }
+//
+// const thePosts = getPosts();
+
+
+export default {
+    async asyncData({ $axios, params }) {
+    console.log(params)
+    const user = params.user // When calling /abc the slug will be "abc"
+    const posts = await $axios.$get(`https://wrfjnsifxl.execute-api.us-east-1.amazonaws.com/production/api/blogs/${user}`)
+    const switches = {}
+    posts.forEach((post) => switches[post._id] = false)
+    return { posts, switches, user }
+    },
+    methods: {
+        switchShow: function (id) {
+            this.switches[id] = !this.switches[id];
+        //     if (this.switches.id == true){
+        //         this.$set(this.switches, id, false)
+        //         console.log('false')
+        //     } else {
+        //         this.$set(this.switches, id, true)
+        //         console.log('true')
+        //     }
+        }
+    }
+
+  // async asyncData ({ params }) {
+  //   const user = params.users // When calling /abc the slug will be "abc"
+  //   return { user }
+  // }
+
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Mada&display=swap');
+
+.container {width: 80%; background-color: #cee0dc; padding: 10px; margin: 10px auto; font-family: 'Mada', sans-serif; border-radius: 10px;}
+
+.container h1 {color: #A5243D; text-transform: uppercase;}
+
+.post {background-color: white; border-radius: 10px; margin: 10px; padding:10px;}
+
+.post h1 {color: black;}
+
+.post h3 {background-color: #A5243D; color: white; max-width: 200px; margin: 10px auto; padding: 5px}
+
+.post p {text-align: left; padding: 20px; font-size: 120%; width: 70%; margin: 10px auto;}
+
+button {margin: 20px; padding: 10px; text-transform: uppercase; width: 100px; background-color:#A5243D;
+        color: white; border: none; transition: color .4s, background-color .4s;}
+
+button:hover {background-color: white; color: #A5243D; }
+
+</style>
